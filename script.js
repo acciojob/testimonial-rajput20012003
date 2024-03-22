486c7127f18dce3e21487a7f9373fd111253c904
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-03-22 12:38:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+body {
+    margin: 0;
+    padding: 0;
+}
+
+.testimonial-container {
+    padding: 20px;
+    background-color: #f5f5f5;
+}
+
+.testimonial {
+    padding: 20px;
+    border: 1px solid #ccc;
+    background-color: #fff;
+    margin-bottom: 20px;
+}
+
+.testimonial-text {
+    margin-bottom: 10px;
+}
+
+.author {
+    text-align: right;
+    font-style: italic;
+}
+
